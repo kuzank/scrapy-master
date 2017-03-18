3f90669a045e767a39ca960a98ad080de76a6b49
Brook <danxiaoguidecade@gmail.com>
2017-03-18 14:55:30 +0800

    scrapy.Field()
    scrapy.Field()

--- wwwSpider/items.py
+++ wwwSpider/items.py
@@ -9,7 +9,6 @@ import scrapy
 WwwspiderItem是一个表示存储信息的类，当爬虫爬下html等信息是，
 可以通过xpath、CSS、beautifulSoup等方式从html中提取信息，并将提取到的信息存放在声明好的item类中，
 
-
 name = scrapy.Field()，声明之后的name可以存储string，数值，dict，列表或者元组等信息,
 可以把name当成一个万能的存储载体就好了
 
